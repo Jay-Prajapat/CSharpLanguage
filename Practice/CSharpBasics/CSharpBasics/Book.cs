922c0ad044dccc8f7fbdf845aee4888239c6741b
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-02-23 18:22:25 +0530

    Learn and practice classes, objects, variable in C#.

---
+++ Practice/CSharpBasics/CSharpBasics/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CSharpBasics
+{
+    internal class Book
+    {
+        public Book(string name) { 
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void showStatstics()
+        {
+           
+            var result = 0.0;
+            var avgGrade = 0.0;
+            var totalGrade = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                lowGrade = Math.Min(lowGrade, number);
+                highGrade = Math.Max(highGrade, number);
+                result += number;
+            }
+            
+            avgGrade = result / grades.Count;
+            Console.WriteLine($"Hightst grade is {highGrade}");
+            Console.WriteLine($"Lowest grade is {lowGrade}");
+
+            Console.WriteLine($"Average Grede is {avgGrade}.");
+            
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
---
+++ Practice/CSharpBasics/CSharpBasics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Reach Dad Poor Dad");
+            book.AddGrade(12.3);
+            book.AddGrade(15.3);
+            book.AddGrade(3.23);
+            book.showStatstics();
+        }
+    }
+}
