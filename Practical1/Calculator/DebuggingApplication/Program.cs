76ca1f6b561581f74a1f484bc5e39c68e1e7f8f8
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-03-02 14:11:15 +0530

    Completed Practical 1

---
+++ Practical1/Calculator/DebuggingApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userResponse = Console.ReadLine();
+            if(int.TryParse(out userResponse)) { 
+                Console.WriteLine("The Entered value is a integer");
+            }
+        }
+    }
+}
