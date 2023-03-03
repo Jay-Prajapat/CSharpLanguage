093610d93368af6e552b4dd60109da85072d7651
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-03-03 18:14:28 +0530

    Completed Inheritance and polymorphism in practical 3.

---
+++ Practical3/Practical3-Inheritance/Practical3-Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3_Inheritance
+{
+    internal class Program
+    {
+        public class Sponser
+        {
+            protected string owner = "Rakuteen";
+        }
+        public class Team : Sponser
+        {
+            private string teamName="Titans";
+            public void printInfo() 
+            {
+                Console.WriteLine($"Team name is {teamName} and the owner is {owner}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Team team = new Team();
+            team.printInfo();
+        }
+    }
+}
---
+++ Practical3/Practical3-Inheritance/Practical3-Polymorphism/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3_Polymorphism
+{
+    internal class Program
+    {
+        public class Brid
+        {
+            //Brid class voice method prints "Turr Turr".
+            public void Voice()
+            {
+                Console.WriteLine("Turr Turr");
+            }
+        }
+        public class Duck : Brid
+        {
+            //Duck class voice method prints "Quack Quack".
+            public void Voice()
+            {
+                Console.WriteLine("Quack Quack");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Brid myBrid = new Brid();
+            Duck myDuck = new Duck();
+            myBrid.Voice();
+            myDuck.Voice();
+        }
+    }
+}
