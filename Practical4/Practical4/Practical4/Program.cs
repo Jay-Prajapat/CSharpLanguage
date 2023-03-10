1c610e4d63fbe16f8f177f9d9877cad1d22ac2e4
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-03-10 18:15:00 +0530

    Completed Practical 4.

---
+++ Practical4/Practical4/Practical4/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Console.WriteLine("Enter name of student.");
+            student1.Name = Console.ReadLine();
+            Console.WriteLine("Enter marks of student.");
+            for (int i = 0; i < 5; i++)
+            {
+                student1.Marks[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            Console.WriteLine("Choose Option from Below :");
+            Console.WriteLine("1.Aggregate:\n2.MinMark\n3.MaxMarks\n4.Grades\n5.Exit");
+            while (true)
+            {
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case (int)Student.Options.Aggregate:
+                        Console.WriteLine($"The name of student is {student1.Name} and the average marks is {student1.CalculateAverageMarks(student1.Marks)}");
+                        break;
+                    case (int)Student.Options.MinMark:
+                        Console.WriteLine($"The Minimum marks is {student1.FindMinimumMarks(student1.Marks)}");
+                        break;
+                    case (int)Student.Options.MaximumMark:
+                        Console.WriteLine($"The Maximum marks is {student1.FindMaximumMarks(student1.Marks)}");
+                        break;
+                    case (int)Student.Options.Grade:
+                        decimal averageMarks = student1.CalculateAverageMarks(student1.Marks);
+                        Console.WriteLine($"The Grade is {student1.CalculateGrade(averageMarks)}");
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+
+                }
+            }
+
+        }
+    }
+    public class Student
+    {
+        public string Name;
+        public decimal[] Marks = new decimal[5];
+        static decimal AverageMarks;
+
+        public decimal CalculateAverageMarks(decimal[] marks)
+        {
+            decimal sum = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                sum += marks[i];
+            }
+            AverageMarks = sum / marks.Length;
+            return AverageMarks;
+        }
+
+        public decimal FindMinimumMarks(decimal[] marks)
+        {
+            return marks.Min();
+        }
+        public decimal FindMaximumMarks(decimal[] marks)
+        {
+            return marks.Max();
+        }
+        public string CalculateGrade(decimal mark)
+        {
+            string grade = "";
+            switch (mark)
+            {
+                case decimal a when mark > 90:
+                    grade = "A";
+                    break;
+                case decimal a when mark > 80:
+                    grade = "B";
+                    break;
+                case decimal a when mark > 70:
+                    grade = "C";
+                    break;
+                case decimal a when mark < 70:
+                    grade = "D";
+                    break;
+                default:
+                    Console.WriteLine("Wrong option selection");
+                    break;
+            }
+            return grade;
+        }
+        public enum Options
+        {
+            Aggregate = 1,
+            MinMark = 2,
+            MaximumMark = 3,
+            Grade = 4
+        }
+    }
+}
