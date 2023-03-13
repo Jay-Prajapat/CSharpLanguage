76766cba9f314f160827c8fd15ade3465d7fc0c3
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-03-13 17:47:30 +0530

    Completed Practical 5

---
+++ Practical5/Practical5/Practical5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < 6; i++)
+            {
+                try
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                catch(IndexOutOfRangeException ex) 
+                {
+                    Console.WriteLine("EXCEPTION: " + ex.Message);    
+                }
+                finally 
+                {
+                    Console.WriteLine("This is finaly block"); 
+                }
+               
+            }
+        }
+    }
+}
