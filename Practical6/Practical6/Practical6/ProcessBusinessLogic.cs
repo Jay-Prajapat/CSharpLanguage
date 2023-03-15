03b720aca296a0b7413ed9160e908b3186b06676
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-03-15 14:15:25 +0530

    Completed Practical 6.

---
+++ Practical6/Practical6/Practical6/ProcessBusinessLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical6
+{
+    public delegate void Notify();
+    public class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started");
+            OnProcessCompleted();
+        }
+
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
---
+++ Practical6/Practical6/Practical6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl  = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted;
+            bl.StartProcess();
+          
+        }
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+}
