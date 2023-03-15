84171cb31cc7d519bf4d7cb274afa9d8d069d4a0
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-03-15 16:24:20 +0530

    Practice About Dependency Inversion Principle

---
+++ Practical7/Practical7/Practical7/IEmployeeDataAccessLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical7
+{
+    public interface IEmployeeDataAccessLogic
+    {
+        Employee GetEmployeeDetails(int id);
+    }
+}
---
+++ Practical7/Practical7/Practical7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeBusinessLogic employeeBusinessLogic= new EmployeeBusinessLogic();
+            Employee emp = employeeBusinessLogic.GetEmployeeDetails(101);
+            Console.WriteLine($"ID: {emp.ID} , Name : {emp.Name}, Department: {emp.Department}, Salary: {emp.Salary}");         
+        }
+    }
+}
