ce1558c482e7be56679249e6c3fd577c0b684e45
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-03-21 18:23:31 +0530

    Implemented customer class with property and implemented admin class.
    In admin class i have implemented add user method.

---
+++ Practical8/Practical8/Practical8/Customer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical8
+{
+    public class Customer : IStudent
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private int _phone;
+        private int _age;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+            }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+            }
+        }
+        public int PhoneNumber
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+        public string GetFullName()
+        {
+                  return this.FirstName + this.LastName;
+        }
+    }
+}
---
+++ Practical8/Practical8/Practical8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical8
+{
+    internal class Program
+    {          
+        static void Main(string[] args)
+        {
+            IStudent student = new Customer();
+            Console.WriteLine("Enter first name of the student");
+            student.FirstName= Console.ReadLine();
+            Console.WriteLine("Enter last name of the student");
+            student.LastName= Console.ReadLine();
+            
+            Console.WriteLine("Enter email of the student");
+            student.Email = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("Enter age of the student");
+                student.Age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter phone number of the student"); 
+                student.PhoneNumber = int.Parse(Console.ReadLine());
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+              
+        }
+    }
+}
